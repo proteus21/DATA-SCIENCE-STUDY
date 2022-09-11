531af86a2eaef937f4fe17d57afb790842b32f29
Boguslaw Konefal <proteus21@wp.pl>
2022-09-11 16:49:08 +0200

    Solution with task

---
+++ Python Exam/1- Initial tasks/max_min_lista.py
@@ -0,0 +1,11 @@
+"""
+    Napisz program, który wyświetla
+    najmniejszy i największy element listy, a także ją odwróci.
+"""
+import math
+import sys
+
+listA=[1,3,56,333]
+print("The smallest value:", min(listA))
+print("The highest value:", max(listA))
+print("Inverted list:",listA[::-1])
