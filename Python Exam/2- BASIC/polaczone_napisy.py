2cae82501ce51bc106b70975f931a9a7b7ebc3c8
Boguslaw Konefal <proteus21@wp.pl>
2022-09-15 22:13:22 +0200

    Task with solution

---
+++ Python Exam/2- BASIC/polaczone_napisy.py
@@ -0,0 +1,27 @@
+"""
+    Napisz funkcje laczaca ze soba dwa napisy w sposob:
+    najpierw pierwsza litera pierwszego napisu, potem pierwsza litera
+    drugiego, druga litera drugiego napisu, itd...
+    Funkcja powinna zwrocic nowy napis bedacy polaczeniem tych podanych
+    jako parametry. Uwaga: wejsciowe napisy nie musza byc tej samej
+    dlugosci!
+    Przyklad:
+    >> merge_strings("pies", "buda")
+    pbiuedsa
+    >> merge_strings("stop", "supermarket)
+    sstuoopermarket
+    W przypadku kiedy jakis napis jest dluzszy, nalezy go po prostu
+    przepisac, podobnie jak to zostalo pokazane na drugim przykladzie.
+"""
+
+
+def merge_strings(string1, string2):
+    """Laczy naprzemiennie napisy string1 oraz string2.
+
+    :param string1: pierwszy napis do polaczenia.
+    :param string2: drugi napis do polaczenia.
+    :return: napis zlozony z podanych jako argumenty.
+
+    """
+    pass
+
